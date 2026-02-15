ae78b90e758372d64b17079e8fff159ce4b04943
chemicallochaain-IN <chemicallochaa.in@gmail.com>
2026-02-16 01:26:13 +0530

    updated formatting of logo

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,36 +1,4 @@
-// Manual type definitions replacing missing vite/client
-
-declare module '*.svg' {
-  import * as React from 'react';
-  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string }>;
-  const path: string;
-  export default path;
-}
-
-declare module '*.png' {
-  const path: string;
-  export default path;
-}
-
-declare module '*.jpg' {
-  const path: string;
-  export default path;
-}
-
-declare module '*.jpeg' {
-  const path: string;
-  export default path;
-}
-
-declare module '*.gif' {
-  const path: string;
-  export default path;
-}
-
-declare module '*.webp' {
-  const path: string;
-  export default path;
-}
+/// <reference types="vite/client" />
 
 interface ImportMetaEnv {
   readonly VITE_SUPABASE_URL: string;
